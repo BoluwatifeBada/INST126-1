a983dc9d390120c372406fdd20b13c57ad83414e
Boluwatife Bada <bbada@terpmail.umd.edu>
2024-11-26 00:00:32 -0500

    created a plan

---
+++ consolidation_project.py
@@ -0,0 +1,11 @@
+# create function to roll
+# check if they tuple out
+# if tupled, player's turn ends with zero points
+# if two are the same, they are fixed and cant be rerolled
+# function for if player wants to stop, add up scores
+# use loop to give players turn until they reach target score
+import random
+
+
+
+    
