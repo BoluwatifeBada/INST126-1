495e5b09246c418ae30950320993c195672ed52b
Boluwatife Bada <bbada@terpmail.umd.edu>
2024-11-26 00:27:04 -0500

    implement the rules

--- consolidation_project.py
+++ consolidation_project.py
@@ -6,6 +6,21 @@
 # use loop to give players turn until they reach target score
 import random
 
+# create function to give players an introduction
+def introduction():
+    print("Welcome to the Tuple out Dice Game")
+    print("Rules:")
+    print("1. The object of the game is to score the most points or reach the target score first.")
+    print("2. Players take turns rolling three dice.")
+    print("3. If all three dice are the same, you 'tuple out' and score 0 points for the turn.")
+    print("4. If two dice are the same, they are 'fixed' and cannot be rerolled.")
+    print("5. You can reroll non-fixed dice as often as you want or stop to score points.")
+    print("6. When you stop, your score is the total of all three dice.")
+    print("7. The game ends when a player reaches the target score and wins.")
+
+
+
+
 
 
     
