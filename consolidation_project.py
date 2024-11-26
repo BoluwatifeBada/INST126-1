3c0cdc41d21fb0a797724273505df63f0f6d0762
Boluwatife Bada <bbada@terpmail.umd.edu>
2024-11-26 01:21:27 -0500

    I have rolled the dice and checked if tuples

--- consolidation_project.py
+++ consolidation_project.py
@@ -18,8 +18,34 @@ def introduction():
     print("6. When you stop, your score is the total of all three dice.")
     print("7. The game ends when a player reaches the target score and wins.")
 
+# function to roll all 3 dice and return their values as a list
+def roll_dice():
+    # roll the three dice
+    die1 = random.randint(1,6)
+    die2 = random.randint(1,6)
+    die3 = random.randint(1,6)
 
+# store the value of the dice in a list
+    dice = [die1, die2, die3]
+    return dice # return the list
 
+# function to calculate the total score of the dice
+def calculate_score(dice):
+    return sum(dice)
+
+# player rolls. implement rules. 
+def turn(player_name):
+    dice = roll_dice()
+    print(f"player_name's turn") 
+    print(f"roll value: {dice}") # display roll value
+
+# check if it tuples
+    if dice[0] == dice[1] == dice[2]: 
+        print("you tupled out! 0 points.")
+        return 0
+    
+# check if fixed
+    fixed_dice = []
 
 
 
